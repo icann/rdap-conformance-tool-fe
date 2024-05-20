adf4f30c98eb290805ce5db6bb2d059c3de18252
Adam Guyot <adam@cobenian.com>
2024-05-20 13:59:03 -0400

    put in a docker file, clean up output and add README notes

--- src/main/java/org/icann/rdapconformancefe/tool/Main.java
+++ src/main/java/org/icann/rdapconformancefe/tool/Main.java
@@ -1,19 +1,17 @@
 package org.icann.rdapconformancefe.tool;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
+import java.util.regex.*;
 import org.icann.rdapconformance.tool.RdapConformanceTool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -37,15 +35,15 @@ public class Main {
   @RestController
   public class SearchController {
 
-    public String findValidationResult(List<String> output) {
-      for (String line : output) {
-        if (line.contains("RDAPValidationResult")) {
-          System.out.println("XXX Found the line: " + line);
-          return line;
-        }
-      }
-      return null; // or some default value
-    }
+    // public String findValidationResult(List<String> output) {
+    //   for (String line : output) {
+    //     if (line.contains("RDAPValidationResult")) {
+    //       System.out.println("XXX Found the line: " + line);
+    //       return line;
+    //     }
+    //   }
+    //   return null; // or some default value
+    // }
 
     @PostMapping("/check")
     @ResponseBody
@@ -54,17 +52,16 @@ public class Main {
         @RequestParam(required = false) String gltdRegistrar,
         @RequestParam(required = false) String gltdRegistry,
         @RequestParam(required = false) String thin) {
-      // LOGGER.info("Received URL: " + url);
-      // LOGGER.info("GltdRegistrar: " + gltdRegistrar);
-      // LOGGER.info("GltdRegistry: " + gltdRegistry);
-      // LOGGER.info("Thin: " + thin);
+      // debug stuff Sysout b/c the redirects of streams messes with Springs logger
       System.out.println("Received URL: " + url);
       System.out.println("GltdRegistrar: " + gltdRegistrar);
       System.out.println("GltdRegistry: " + gltdRegistry);
       System.out.println("Thin: " + thin);
+      // we do nothing with the options for now, logic for that comes next
 
       // Get the RDPT directory from the environment
       String rdpt = System.getenv("RDPT");
+      Pattern pattern = Pattern.compile(".*  : (.*)$");
 
       // Construct the arguments
       String[] args =
@@ -77,11 +74,7 @@ public class Main {
             url
           };
 
-      // Create a new CommandLine instance
-      // CommandLine cmd = new CommandLine(new RdapConformanceTool());
-      // Execute the RdapConformanceTool with the arguments
-      // int exitCode = cmd.execute(args);
-      // We shouldn't need a Callable that will execute the command
+      // We shouldn't need a Callable, Spring runs in a separate thread
       String resultsFile = null;
       try {
         // Create a new ByteArrayOutputStream to capture the output
@@ -111,7 +104,17 @@ public class Main {
         // output.forEach(System.out::println);
 
         for (String line : output) {
-          System.out.println("<OUTPUT> " + line);
+          String lineWithoutAnsi = line.replaceAll("\u001B\\[[;\\d]*m", "");
+          // System.out.println("<OUTPUT> " + line);
+          Matcher matcher = pattern.matcher(lineWithoutAnsi);
+          if (matcher.find()) {
+            String message = matcher.group(1);
+            if (!message.startsWith("X509Certificate:")) {
+              // System.out.println("!Found!: " + message);
+            }
+          } else {
+            // System.out.println("XXMISS<<" + lineWithoutAnsi + ">>");
+          }
         }
         for (String line : output) {
           // System.out.println(line);
@@ -138,7 +141,6 @@ public class Main {
           System.out.println("Got the file contents.");
           // System.out.println("Results file content: " + fileContent);
         } catch (IOException e) {
-          // LOGGER.error("Error reading file: " + resultsFile, e);
           System.out.println("Results file error: " + e.getMessage());
           resultMap.put("data", "error");
         }
@@ -148,84 +150,6 @@ public class Main {
 
       System.out.println("Processed is finished, all set to return..");
       return resultMap;
-    } // end of post
-
-    @PostMapping("/old")
-    @ResponseBody
-    public Map<String, String> old(@RequestParam String url) {
-      LOGGER.info("OLD Received URL: " + url);
-
-      // Get the RDPT directory from the environment
-      String rdpt = System.getenv("RDPT");
-      String rdapct = System.getenv("RDAPCT");
-
-      // Construct the command
-      List<String> command =
-          Arrays.asList(
-              "java",
-              "-jar",
-              rdapct + "/tool/target/rdapct-1.0.2.jar",
-              "--use-local-datasets",
-              // "-v",
-              "--print-results-path",
-              "-c",
-              // rdpt + "/tool/bin/rdapct-config.json",
-              rdpt + "/rdapct-config.json",
-              url);
-      try {
-        // Run the command
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
-        processBuilder.redirectErrorStream(true); // Redirect error stream to output stream
-        Process process = processBuilder.start();
-
-        // Create a reader for the output stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-        // Create a list to store lines
-        List<String> lines = new ArrayList<>();
-
-        // Read the output and error streams
-        String line;
-        while ((line = reader.readLine()) != null) {
-          System.out.println(line); // Write to console
-          lines.add(line); // Add line to list
-        }
-
-        // Wait for the process to finish
-        int exitCode = process.waitFor();
-
-        // Process the lines
-        String result =
-            lines
-                .stream()
-                .filter(l -> l.contains("RDAPValidationResult{") && l.contains("}"))
-                .findFirst()
-                .orElse("unknown");
-
-        // Extract the code
-        if (result.contains("RDAPValidationResult{")) {
-          result = result.substring(result.indexOf("RDAPValidationResult{"));
-        }
-
-        // Create a map to store the result
-        Map<String, String> resultMap = new HashMap<>();
-        resultMap.put("data", result);
-        LOGGER.info("OLD Processed is finished, all set to return..");
-        return resultMap;
-
-      } catch (IOException e) {
-        LOGGER.severe("Failed to run command: " + e.getMessage());
-      } catch (InterruptedException e) {
-        Thread.currentThread().interrupt(); // restore interrupted status
-        LOGGER.severe("Thread was interrupted: " + e.getMessage());
-      }
-      // Create a map to store the error message
-      Map<String, String> errorMap = new HashMap<>();
-      errorMap.put("data", "bad");
-
-      LOGGER.info("OLD Processed is finished, bad error!");
-
-      return errorMap;
     } // end of post
   } // end of class
 } // end of main
