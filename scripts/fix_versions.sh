ea72bbf89440ae9c0e474cb0eda08d4426fc5104
Adam Guyot <adam@cobenian.com>
2024-06-19 11:20:19 -0400

    dont be space dependant

--- scripts/fix_versions.sh
+++ scripts/fix_versions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # Extract the version number from the filename
 VERSION=$(echo $1 | awk -F- '{print $2}' | awk -F. '{print $1"."$2"."$3}')
 
-# Define the old and new dependency strings
-OLD_DEPENDENCY='<dependency>\n      <groupId>org.icann</groupId>\n      <artifactId>rdap-conformance</artifactId>\n      <version>.*</version>\n    </dependency>'
+# Define the old and new dependency strings with improved regex for whitespace
+OLD_DEPENDENCY='<dependency>\n\s*<groupId>org\.icann</groupId>\n\s*<artifactId>rdap-conformance</artifactId>\n\s*<version>.*</version>\n\s*</dependency>'
 NEW_DEPENDENCY="<dependency>\n      <groupId>org.icann</groupId>\n      <artifactId>rdap-conformance</artifactId>\n      <version>$VERSION</version>\n    </dependency>"
 
-# Replace the old dependency with the new one in the pom.xml file
+# Replace the old dependency with the new one in the pom.xml file, handling any amount of whitespace
 sed -i "s|$OLD_DEPENDENCY|$NEW_DEPENDENCY|g" /rdap-conformance-tool-fe/pom.xml
